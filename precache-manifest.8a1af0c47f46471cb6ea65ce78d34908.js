7f3a228eb0b8616df6cfc2995def0f5b59f61214
indmind <sattanationmail@gmail.com>
2020-03-18 05:09:41 +0700

    deploy

---
+++ precache-manifest.8a1af0c47f46471cb6ea65ce78d34908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4f9401fa1240877bdd",
+    "url": "/css/app.35157df1.css"
+  },
+  {
+    "revision": "fc2c1edbaacfa70b9d7e",
+    "url": "/css/chunk-vendors.3e720d07.css"
+  },
+  {
+    "revision": "c5e7c12b0e742f7ff51c5027f0b258f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f4f9401fa1240877bdd",
+    "url": "/js/app.8f11e084.js"
+  },
+  {
+    "revision": "fc2c1edbaacfa70b9d7e",
+    "url": "/js/chunk-vendors.7971459e.js"
+  },
+  {
+    "revision": "6c03e7aa42187854e2a49b899408c326",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
